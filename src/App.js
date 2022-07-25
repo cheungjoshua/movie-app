81af831a09e92cbbb25545bf5659a0522bb8fbb1
Joshua Cheung <contact.cjoshua@gmail.com>
2022-07-25 14:27:22 -0700

    Succes API call and recieve data from TMDB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
+import "./App.css";
+
+const API_KEY = "ed4fecd45ab8041d6f5b86745794548c";
+const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=";
+const SEARCH = "https://api.themoviedb.org/3/search/company?api_key=";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_URL + API_KEY).then((res) => {
+      console.log(res.data);
+      setMovies(res.data.results);
+      console.log(movies);
+    });
+  }, []);
+
+  const movieCard = movies.map((data) => {
+    return <MovieCard />;
+  });
+
+  return <div className="App">{movieCard}</div>;
 }
 
 export default App;
