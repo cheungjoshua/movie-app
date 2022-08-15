bd4af519c2ecd1752d5bf03469bdcdbe74da6f5c
Joshua Cheung <contact.cjoshua@gmail.com>
2022-08-15 14:07:33 -0700

    code refactor, add Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import MovieCard from "./MovieCard";
+
 import "./App.scss";
+import Nav from "./components/Nav";
+import MovieCard from "./MovieCard";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = "https://api.themoviedb.org/3";
@@ -9,8 +11,6 @@ const SEARCH = "https://api.themoviedb.org/3/search/company";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [genres, setGenres] = useState([{ id: 0, name: "None" }]);
-  const [value, setValue] = useState("None");
 
   // fetch movie
   const fetchMovie = async () => {
@@ -23,31 +23,8 @@ function App() {
     setMovies(results);
   };
 
-  // Fetch Genres
-  const fetchGenres = async () => {
-    const {
-      data: { genres },
-    } = await axios.get(`${API_URL}/genre/movie/list`, {
-      params: { api_key: API_KEY },
-    });
-
-    setGenres((prev) => [...prev, ...genres]);
-  };
-
-  // Fetch by genres
-  const fetchByGenres = async (id) => {
-    const {
-      data: { results },
-    } = await axios.get(`${API_URL}/discover/movie`, {
-      params: { api_key: API_KEY, with_genres: id },
-    });
-    console.log(results);
-    setMovies(results);
-  };
-
   useEffect(() => {
     fetchMovie();
-    fetchGenres();
   }, []);
 
   // Create indivials movie card components
@@ -55,25 +32,10 @@ function App() {
     return <MovieCard key={data.id} {...data} />;
   });
 
-  // Create Genres drop down component
-  const genresList = genres.map((genre) => {
-    return <option value={genre.id}>{genre.name}</option>;
-  });
-
   return (
     <div className="App">
-      <nav>
-        <select
-          value={value}
-          onChange={(e) => {
-            e.preventDefault();
-            fetchByGenres(e.target.value);
-            console.log("called");
-          }}
-        >
-          {genresList}
-        </select>
-      </nav>
+      <Nav setMovies={setMovies} />
+
       <div className="movies_container">{movieCard}</div>
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = "https://api.themoviedb.org/3";
+
+export default function Nav({ setMovies }) {
+  const [genres, setGenres] = useState([{ id: 0, name: "None" }]);
+  const [value, setValue] = useState("None");
+
+  // Fetch Genres
+  const fetchGenres = async () => {
+    const {
+      data: { genres },
+    } = await axios.get(`${API_URL}/genre/movie/list`, {
+      params: { api_key: API_KEY },
+    });
+
+    setGenres((prev) => [...prev, ...genres]);
+  };
+
+  // Fetch by genres
+  const fetchByGenres = async (id) => {
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/discover/movie`, {
+      params: { api_key: API_KEY, with_genres: id },
+    });
+    console.log(results);
+    setMovies(results);
+  };
+
+  useEffect(() => {
+    fetchGenres();
+  }, []);
+
+  // Create Genres drop down component
+  const genresList = genres.map((genre) => {
+    return <option value={genre.id}>{genre.name}</option>;
+  });
+
+  return (
+    <nav>
+      <select
+        value={value}
+        onChange={(e) => {
+          e.preventDefault();
+          fetchByGenres(e.target.value);
+          console.log("called");
+        }}
+      >
+        {genresList}
+      </select>
+    </nav>
+  );
+}
